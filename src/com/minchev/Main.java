54fa8e98648be48052508e53054994546337e26b
Georgi Minchev <georgi.minchev@gmail.com>
2019-11-26 16:37:37 +0200

    add a new method

--- src/com/minchev/Main.java
+++ src/com/minchev/Main.java
@@ -13,20 +13,22 @@ public class Main {
                 "\n" +
                 "            Edit the Expression & Text to see matches. [b]Roll over matches or the expression for [b]details[/b]. PCRE & JavaScript flavors of RegEx are supported.\n" +
                 "\n";
-        str +=str;
+        //str +=str;
 
         StringBuilder b = new StringBuilder(str);
-        findBoldText(b);
+        //findBoldText(b);
         findBoldTextWithIndexOf(str);
-
+        System.out.printf("==============");
+        findBoldTextWithIndexOf2(str);
     }
+
+
+
 
 
     public static void findBoldText(StringBuilder template) {
         long start = System.currentTimeMillis();
         System.out.println("Start : " + start);
-
-        System.out.println("Start : " + System.currentTimeMillis());
 
         String regexPattern = "(\\[b\\])[а-яА-Я\\w\\s\\d.+ $ { }№: ,  \\/()]+(\\[\\/b\\])";
         String regexBold = "(\\[b\\])";
@@ -51,9 +53,57 @@ public class Main {
         System.out.println("Result : " + (stop-start));
     }
 
-    public static void findBoldTextWithIndexOf(String str) {
+
+    public static void findBoldTextWithIndexOf2(String str) {
         long start = System.currentTimeMillis();
         System.out.println("Start : " + start);
+
+        StringBuilder result = new StringBuilder();
+        boolean flag = false;
+        int openIndex = 0;
+        String pieceOfString;
+        String pieceOfReplace;
+        int closeIndex = 0;
+        int nextIndex = 0;
+        while (flag == false) {
+            openIndex = str.indexOf("[b]");
+            closeIndex = str.indexOf("[/b]");
+            if (openIndex > -1 && closeIndex > -1 && openIndex<closeIndex) {
+                closeIndex += 4;
+                if (openIndex>0) openIndex = 0;
+                pieceOfReplace = str.substring(openIndex, closeIndex);
+                pieceOfReplace = pieceOfReplace.replaceFirst("\\[b\\]", "<b>");
+                pieceOfReplace = pieceOfReplace.replaceFirst("\\[/b\\]", "</b>");
+                str = str.substring(closeIndex, str.length());
+                result.append(pieceOfReplace);
+                nextIndex= closeIndex;
+            }
+
+            if (openIndex > -1 && closeIndex > -1 && openIndex>closeIndex) {
+                pieceOfString = str.substring(0, openIndex-1);
+                str = str.substring(openIndex, str.length());
+                result.append(pieceOfString);
+                nextIndex= openIndex-1;
+            }
+
+
+            if (openIndex == -1 && closeIndex == -1) {
+                flag = true;
+                //pieceOfString = str;
+                result.append(str);
+                //result.append(str);
+            }
+        }
+        System.out.println(result);
+        long stop = System.currentTimeMillis();
+        System.out.println("Stop : " + stop);
+        System.out.println("Result : " + (stop-start));
+    }
+
+
+    public static void findBoldTextWithIndexOf(String str) {
+        long start = System.currentTimeMillis();
+        System.out.println("Start  1: " + start);
         boolean flag = false;
         String substring;
         int closeIndex = 0;
