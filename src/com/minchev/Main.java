e09b5a0575c523a1b7d903d54efd7052ba31246c
georgi <georgi.minchev@gmail.com>
2019-11-29 17:08:25 +0200

    final method

--- src/com/minchev/Main.java
+++ src/com/minchev/Main.java
@@ -12,22 +12,28 @@ public class Main {
     public static void main(String[] args) throws IOException {
         // write your code here
         BigDecimal amount = new BigDecimal(21.21);
-        String str = "RegExr Акт за установяване на административно нарушение (АУАН) [/b]№ 1400000057/22.11.2019г.[b]  was [b]created { 12.12.2020[/b] by gskinner.com, and is proudly hosted by Media Temple.\n" +
+        String str = "RegExr Акт за установяване на административно нарушение (АУАН) [/b]№ 1400000057/22.11.2019г.[b]  was [b]created { 12.12.2020[/b] by [b]gskinner.com[/b], and is [b]proudly[/b] hosted by Media Temple.\n" +
                 "\n" +
                 "            Edit the Expression & Text to see matches. [b]Roll over matches or the expression for [b]details[/b]. PCRE & JavaScript flavors of RegEx are supported.\n" +
                 "\n";
         //str +=str;
 
         StringBuilder b = new StringBuilder(str);
-        //findBoldText(b);
-        findBoldTextWithIndexOf(str);
+       //findBoldText(b);
+        System.out.printf("===== 1=========");
+       findBoldTextWithIndexOf(str);
         System.out.printf("===== 2=========");
-        findBoldTextWithIndexOf2(str);
-        System.out.printf("=======3=======");
-        temp3(str);
+
+        findBoldTextWithIndexOfFinal(str);
+
+      /*  findBoldTextWithIndexOf2(str);
+       System.out.printf("=======3=======");
+        temp3(str);*/
+
+
     }
 
-    public static void findBoldText(StringBuilder template) {
+  /*  public static void findBoldText(StringBuilder template) {
         long start = System.currentTimeMillis();
         System.out.println("Start : " + start);
 
@@ -52,18 +58,47 @@ public class Main {
         long stop = System.currentTimeMillis();
         System.out.println("Stop : " + stop);
         System.out.println("Result : " + (stop - start));
+    }*/
+
+
+    public static void findBoldTextWithIndexOfFinal(String str) {
+        StringBuilder result = new StringBuilder();
+        int openIndex = str.indexOf("[b]");
+        int closeIndex;
+        String pieceOfString;
+
+        while (openIndex >= 0) {
+            closeIndex = 0;
+            result.append(str.substring(closeIndex, openIndex));
+            closeIndex = str.indexOf("[/b]", openIndex);
+            if (closeIndex == -1) {
+                str = str.substring(openIndex);
+                break;
+            }
+            closeIndex += 4;
+            pieceOfString = str.substring(openIndex, closeIndex);
+            pieceOfString = pieceOfString.replaceFirst("\\[b\\]", "<b>");
+            pieceOfString = pieceOfString.replaceFirst("\\[/b\\]", "</b>");
+            result.append(pieceOfString);
+
+            str = str.substring(closeIndex);
+            openIndex = str.indexOf("[b]");
+        }
+
+        if (str.length() > 0) {
+            result.append(str);
+        }
+        System.out.println(result);
     }
 
 
     public static void findBoldTextWithIndexOf(String str) {
-        long start = System.currentTimeMillis();
-        System.out.println("Start  1: " + start);
         boolean flag = false;
         String substring;
         int closeIndex = 0;
         int openIndex = 0;
         int nextIndex = 0;
-        int cOpenIndex;
+
         StringBuilder result = new StringBuilder();
         while (flag == false) {
             openIndex = str.indexOf("[b]", closeIndex);
@@ -77,11 +112,11 @@ public class Main {
             if (openIndex >= -1 && closeIndex >= -1 && openIndex < closeIndex) {
                 closeIndex += 4;
                 substring = str.substring(openIndex, closeIndex);
-                cOpenIndex = substring.lastIndexOf("[b]");
+                nextIndex = substring.lastIndexOf("[b]");
 
-                if (cOpenIndex > 0) {
-                    result.append(substring.substring(0, cOpenIndex));
-                    openIndex += cOpenIndex;
+                if (nextIndex > 0) {
+                    result.append(substring, 0, nextIndex);
+                    openIndex += nextIndex;
                     substring = str.substring(openIndex, closeIndex);
                 }
                 String s = substring.replace("[b]", "<b>");
@@ -95,12 +130,12 @@ public class Main {
             //
             if (openIndex > -1 && closeIndex == -1) {
                 flag = true;
-                result.append(str.substring(openIndex, str.length()));
+                result.append(str.substring(openIndex));
             }
 
             if (openIndex == -1 && closeIndex == -1) {
                 flag = true;
-                result.append(str.substring(nextIndex, str.length()));
+                result.append(str.substring(nextIndex));
             }
         }
         System.out.println(result);
@@ -121,9 +156,12 @@ public class Main {
             closeIndex = str.indexOf("[/b]");
             if (openIndex > -1 && closeIndex > -1 && openIndex < closeIndex) {
                 closeIndex += 4;
-
                 if (openIndex > 0) openIndex = 0;
+
+                //searchingWorkString(String str);
+
                 pieceOfString = str.substring(openIndex, closeIndex);
+
                 pieceOfString = pieceOfString.replaceFirst("\\[b\\]", boldStart);
                 pieceOfString = pieceOfString.replaceFirst("\\[/b\\]", boldEnd);
                 str = str.substring(closeIndex);
@@ -142,12 +180,9 @@ public class Main {
             }
         }
         System.out.println(result);
-
     }
 
     public static void temp3(String template) throws IOException {
-
-
         BufferedReader reader = new BufferedReader(new StringReader(template));
         StringBuilder result = new StringBuilder();
         String line;
@@ -182,6 +217,7 @@ public class Main {
                 openIndex = 0;
             }
             pieceOfString   =s;
+
             str =  str.replace( str.substring(openIndex, closeIndex), pieceOfString);
             return searchingWorkString(str);
         }
